767b0518cddd2937df0c1afd844983cb67aa6650
DeRyckeThomas <thomas.de.rycke@student.howest.be>
2018-02-19 10:08:10 +0100

    html aangemaakt

--- lesvb2/Startup.cs
+++ lesvb2/Startup.cs
@@ -25,9 +25,12 @@ namespace lesvb2
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello world!");
             });
         }
     }
